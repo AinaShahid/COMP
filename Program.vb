5cc35e824331d28310dd142d5d540dc4c0e8ce9a
AinaShahid <152895554+AinaShahid@users.noreply.github.com>
2023-12-10 20:38:03 +0500

    fix start

--- Program.vb
+++ Program.vb
@@ -1,48 +1,22 @@
 Imports System
-Imports System.Threading
 
 Module Program
     Sub Main()
-        'inputtwo strings and swap two chars of both string
-        Dim str1, str2, Newstr1, Newstr2, MX As String
-        Dim i, k As Integer
-        Dim chr1, chr2, X, Y As String
-        i = 0
-        k = 0
-        str1 = " "
-        str2 = " "
-        Newstr1 = " "
-        Newstr2 = " "
-        MX = " "
-        chr1 = " "
-        chr2 = " "
-        X = " "
-        Y = " "
-        Console.WriteLine("enter the word: ")
-        str1 = Console.ReadLine()
-        Console.WriteLine("enter the second word: ")
-        str2 = Console.ReadLine()
-        i = Len(str1)
-        k = Len(str2)
-        If i >= 2 Then
-            chr1 = Left(str1, 2)
-            If k >= 2 Then
-                chr2 = Left(str2, 2)
+        Dim S As String
+        Dim chr As Char
+        Dim chr2 As Char
+        Dim j As Integer
+        Console.WriteLine("enter the word")
+        S = Console.ReadLine()
+        chr = Left(S, 1)
+        Console.WriteLine(chr)
+        For j = 1 To Len(S) - 1
+            chr2 = Mid(S, (j + 1), 1)
+            If chr2 = chr Then
+                chr2 = "*"
             End If
-        End If
-        If i > 2 Then
-            X = Right(str1, i - 2)
-            If k > 2 Then
-                Y = Right(str2, k - 2)
-            End If
-        End If
-
-        Newstr1 = chr1 & Y
-        Newstr2 = chr2 & X
-
-        MX = Newstr1 & " " & Newstr2
-        Console.WriteLine("the new string is " & MX)
+            Console.WriteLine(chr2)
+        Next
         Console.ReadKey()
-
     End Sub
 End Module
