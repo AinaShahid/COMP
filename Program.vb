0b5df0450c8b7d1bddc76df66a9bb401e48146ad
AinaShahid <152895554+AinaShahid@users.noreply.github.com>
2023-12-10 20:33:01 +0500

    Add files via upload
    dounts

--- Program.vb
+++ Program.vb
@@ -2,25 +2,15 @@ Imports System
 
 Module Program
     Sub Main()
-        Dim Str1 As String
-        Dim j, vowel As Integer
-        Dim CHR As Char
-        j = 0
-        vowel = 0
-        Console.WriteLine("Enter string to find vowels from it: ")
-        Str1 = UCase(Console.ReadLine())
-        For j = 1 To Len(Str1)
-            CHR = Mid(Str1, j, 1)
-            If CHR = "A" Or CHR = "E" Or CHR = "I" Or CHR = "O" Or CHR = "U" Then
-                vowel = vowel + 1
-            End If
-        Next
-        Console.WriteLine("number of vowels in the string:  " & vowel)
-        Console.ReadKey()
-
-
-
-
+        Dim Dounts As Integer
+        Dounts = 0
+        Console.WriteLine("enter the num of donuts: ")
+        Dounts = Console.ReadLine()
+        If Dounts >= 10 Then
+            Console.WriteLine("number of dounts: many ")
+        Else
+            Console.WriteLine("number of dounts: " & Dounts)
+        End If
 
 
 
