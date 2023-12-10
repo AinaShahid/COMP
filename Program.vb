de74269a408e962e666166d2cb1acef04687921b
AinaShahid <152895554+AinaShahid@users.noreply.github.com>
2023-12-10 20:24:44 +0500

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -1,26 +1,36 @@
 Imports System
-Imports System.ComponentModel.Design
 
 Module Program
     Sub Main()
-        Dim str, Str2 As String
-        Dim chr, chr2 As String
-
-        Str2 = " "  ' variable which stores the string after any addition or no changes 
-        str = " "
-        chr = "ing "
-        chr2 = "ly"
-        Console.WriteLine("enter string:")
-        str = Console.ReadLine()
-        If Len(str) >= 3 And Right(str, 3) = "ing" Then
-            Str2 = str & chr2
-        ElseIf Len(str) >= 3 Then
-            Str2 = str & chr
-        Else
-            Str2 = str
-        End If
-        Console.WriteLine("the new string is: " & Str2)
+        Dim Str1 As String
+        Dim NumCount, i, k, ANum As Integer
+        Dim Chr, chr2, Chr3 As Char
+        ANum = 0
+        Chr3 = " "
+        NumCount = 0
+        Console.WriteLine("Enter the strin: ")
+        Str1 = Console.ReadLine()
+        For k = 1 To Len(Str1)
+            Chr = Mid(Str1, k, 1)
+            NumCount = 0
+            For i = 1 To Len(Str1)
+                chr2 = Mid(Str1, i, 1)
+                If Chr = chr2 Then NumCount = NumCount + 1
+            Next
+            If NumCount > ANum Then
+                ANum = NumCount
+                Chr3 = chr2
+            End If
+        Next
+        Console.WriteLine(Chr3 & " : appeared" & ANum & "  : times")
         Console.ReadKey()
+
+
+
+
+
+
+
 
     End Sub
 End Module
