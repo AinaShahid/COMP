5f1301cc44543d26ee9aa77fa974e5cd21d5833d
AinaShahid <152895554+AinaShahid@users.noreply.github.com>
2023-12-10 20:36:13 +0500

    program 11

--- Program.vb
+++ Program.vb
@@ -1,32 +1,23 @@
 Imports System
-Imports System.Transactions
 
 Module Program
     Sub Main()
-        Dim Str1, Str2 As String
-        Dim char1, char2, char3 As Char
-        Dim i As Integer
-        Str1 = ""
-        Str2 = ""
-        char1 = ""
-        char2 = ""
-        char3 = ""
-        i = 0
-        Console.WriteLine("Enter the string: ")
-        Str1 = Console.ReadLine()
-        Console.WriteLine("Enter the character to be replaced: ")
-        char1 = Console.ReadLine()
-        Console.WriteLine("Enter character to be replaced with: ")
-        char2 = Console.ReadLine()
-        For i = 1 To Len(Str1)
-            char3 = Mid(Str1, i, 1)
-            If char1 <> char3 Then
-                Str2 = Str2 & char3
-            Else
-                Str2 = Str2 & char2
-            End If
-        Next
-        Console.WriteLine("The new string is: " & Str2)
+        Dim str1, Nstr As String
+        Dim chr, chr2, chr3 As String
+        Nstr = " "
+        str1 = " "
+        chr = InStr(str1, "not")
+        chr2 = InStr(str1, "bad ")
+        chr3 = "good"
+        Console.WriteLine("enter string: ")
+        str1 = Console.ReadLine()
+        If chr & chr2 = "not bad" Then
+            Nstr = str1 & chr3
+        End If
+        Console.WriteLine(Nstr)
         Console.ReadKey()
+
+
+
     End Sub
 End Module
