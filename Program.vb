4d25484b57f0d51c2107fdc6f45a881c6e39c214
AinaShahid <152895554+AinaShahid@users.noreply.github.com>
2023-12-10 20:30:07 +0500

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -3,28 +3,20 @@ Imports System
 Module Program
     Sub Main()
         Dim Str1 As String
-        Dim NumCount, i, k, ANum As Integer
-        Dim Chr, chr2, Chr3 As Char
-        ANum = 0
-        Chr3 = " "
-        NumCount = 0
-        Console.WriteLine("Enter the strin: ")
-        Str1 = Console.ReadLine()
-        For k = 1 To Len(Str1)
-            Chr = Mid(Str1, k, 1)
-            NumCount = 0
-            For i = 1 To Len(Str1)
-                chr2 = Mid(Str1, i, 1)
-                If Chr = chr2 Then NumCount = NumCount + 1
-            Next
-            If NumCount > ANum Then
-                ANum = NumCount
-                Chr3 = chr2
+        Dim j, vowel As Integer
+        Dim CHR As Char
+        j = 0
+        vowel = 0
+        Console.WriteLine("Enter string to find vowels from it: ")
+        Str1 = UCase(Console.ReadLine())
+        For j = 1 To Len(Str1)
+            CHR = Mid(Str1, j, 1)
+            If CHR = "A" Or CHR = "E" Or CHR = "I" Or CHR = "O" Or CHR = "U" Then
+                vowel = vowel + 1
             End If
         Next
-        Console.WriteLine(Chr3 & " : appeared" & ANum & "  : times")
+        Console.WriteLine("number of vowels in the string:  " & vowel)
         Console.ReadKey()
-
 
 
 
